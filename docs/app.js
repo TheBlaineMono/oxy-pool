5e0a2bd804bdb109a40ce4d66473f21d9d397459
TheBlaineMono <32774919+TheBlaineMono@users.noreply.github.com>
2017-10-25 17:19:36 +0200

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -17,7 +17,7 @@ app.controller('indexCtrl', function($scope, $http) {
         }
     });
 
-    $http.get ('https://wallet.oxycoin.io/api/delegates/get?username=500bitcoins').then (function (res) {
+    $http.get ('https://twallet.oxycoin.io/api/delegates/get?username=theblainemono').then (function (res) {
         $scope.delegate = res.data.delegate;
     });
 });
